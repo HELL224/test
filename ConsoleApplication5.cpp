6425a6e7587df781382338008f10f0ae6a2f06c5
Sasha <moloko371a@gmail.com>
2024-12-10 19:13:06 +0200

    func_3

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -6,12 +6,7 @@ using namespace std;
 int main()
 {
     cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-}
+    
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
